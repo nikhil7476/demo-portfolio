cd50409e66ae08e797ce2c2dab13d1cf318b88b0
nikhil7476 <nikhil9027917476@gmail.com>
2025-10-27 19:40:39 +0530

    modified:   src/pages/our-products/[id].js

--- src/pages/our-products/[id].js
+++ src/pages/our-products/[id].js
@@ -31,110 +31,184 @@ function ProductDetail() {
       <Banner data={bannerData.products[id]} />
 
       {/* Section - 1 About Product Section */}
-      <section>
-        <Container>
-          <Row>
-            <Col xl={6} lg={6} md={6} sm={12} xs={12} className="mb-3">
-              <Image
-                src={product.section1.sectionImage}
-                alt="ABout Product"
-                title="About Product"
-                width={1000}
-                height={600}
-                style={{ width: "100%", height: "auto" }}
-                className="img-fluid rounded"
-              />
-            </Col>
-            <Col xl={6} lg={6} md={6} sm={12} xs={12} className="mb-3">
-              <span>{product.section1.subheading}</span>
-              <h2>{product.section1.title}</h2>
-              <hr />
-              <p>{product.section1.description}</p>
+      {product.section1 && (
+        <section>
+          <Container>
+            <Row>
+              <Col xl={6} lg={6} md={6} sm={12} xs={12} className="mb-3">
+                {product.section1.sectionImage && (
+                  <Image
+                    src={product.section1.sectionImage}
+                    alt="About Product"
+                    title="About Product"
+                    width={1000}
+                    height={600}
+                    style={{ width: "100%", height: "auto" }}
+                    className="img-fluid rounded"
+                  />
+                )}
+              </Col>
+
+              <Col xl={6} lg={6} md={6} sm={12} xs={12} className="mb-3">
+                {product.section1.subheading && (
+                  <span>{product.section1.subheading}</span>
+                )}
+                {product.section1.title && <h2>{product.section1.title}</h2>}
+                {(product.section1.subheading || product.section1.title) && (
+                  <hr />
+                )}
+                {product.section1.description && (
+                  <p>{product.section1.description}</p>
+                )}
+
+                {product.section1.featuredPoints?.length > 0 && (
+                  <Row>
+                    {product.section1.featuredPoints.map((point, index) => (
+                      <Col
+                        key={index}
+                        xl={6}
+                        lg={6}
+                        md={6}
+                        sm={12}
+                        xs={12}
+                        className="mb-3"
+                      >
+                        <span className="me-2">
+                          <FaCheckCircle />
+                        </span>{" "}
+                        {point}
+                      </Col>
+                    ))}
+                  </Row>
+                )}
+              </Col>
+            </Row>
+
+            {product.section1.ctaLink && product.section1.ctaText && (
               <Row>
-                {product.section1.featuredPoints &&
-                  product.section1.featuredPoints.length > 0 &&
-                  product.section1.featuredPoints.map((point, index) => (
-                    <Col
-                      key={index}
-                      xl={6}
-                      lg={6}
-                      md={6}
-                      sm={12}
-                      xs={12}
-                      className="mb-3"
-                    >
-                      <span className="me-2">
-                        <FaCheckCircle />
-                      </span>{" "}
-                      {point}
-                    </Col>
-                  ))}
+                <Col className="text-center">
+                  <Link
+                    href={product.section1.ctaLink}
+                    title={product.section1.ctaText}
+                    className="ctaButton"
+                  >
+                    {product.section1.ctaText} <FaArrowRightLong />
+                  </Link>
+                </Col>
               </Row>
-            </Col>
-          </Row>
-          <Row>
-            <Col className="text-center">
-              <Link
-                href={product.section1.ctaLink}
-                title={product.section1.ctaText}
-                className="ctaButton"
-              >
-                {product.section1.ctaText} <FaArrowRightLong />
-              </Link>
-            </Col>
-          </Row>
-        </Container>
-      </section>
+            )}
+          </Container>
+        </section>
+      )}
 
       {/* Section - 2 Core Highlight Section */}
-      <section>
-        <Container>
-          <Row>
-            <Col>
-              <span>{product.section2.subheading}</span>
-              <h2>{product.section2.heading}</h2>
-              <hr />
-              <p>{product.section2.description}</p>
-            </Col>
-          </Row>
-          <Row>
-            {product.section2.iconBox &&
-              product.section2.iconBox.length > 0 &&
-              product.section2.iconBox.map((iconBox, index) => (
-                <Col
-                  key={index}
-                  xl={4}
-                  lg={4}
-                  md={6}
-                  sm={12}
-                  xs={12}
-                  className="mb-3"
-                >
-                  <div className="h-100 d-flex gap-3">
-                    <div>
-                      <span className="fs-2">{iconBox.icon}</span>
+      {product.section2 && (
+        <section>
+          <Container>
+            <Row>
+              <Col>
+                <span>{product.section2.subheading}</span>
+                <h2>{product.section2.heading}</h2>
+                <hr />
+                <p>{product.section2.description}</p>
+              </Col>
+            </Row>
+
+            {product.section2.iconBox?.length > 0 && (
+              <Row>
+                {product.section2.iconBox.map((iconBox, index) => (
+                  <Col
+                    key={index}
+                    xl={4}
+                    lg={4}
+                    md={6}
+                    sm={12}
+                    xs={12}
+                    className="mb-3"
+                  >
+                    <div className="h-100 d-flex gap-3">
+                      <div>
+                        <span className="fs-2">{iconBox.icon}</span>
+                      </div>
+                      <div>
+                        <h3>{iconBox.title}</h3>
+                        <p>{iconBox.description}</p>
+                      </div>
                     </div>
-                    <div>
-                      <h3>{iconBox.title}</h3>
-                      <p>{iconBox.description}</p>
-                    </div>
-                  </div>
+                  </Col>
+                ))}
+              </Row>
+            )}
+
+            {product.section2.ctaLink && product.section2.ctaText && (
+              <Row>
+                <Col className="text-center">
+                  <Link
+                    href={product.section2.ctaLink}
+                    title={product.section2.ctaText}
+                    className="ctaButton"
+                  >
+                    {product.section2.ctaText} <FaArrowRightLong />
+                  </Link>
                 </Col>
-              ))}
-          </Row>
-          <Row>
-            <Col className="text-center">
-              <Link
-                href={product.section2.ctaLink}
-                title={product.section2.ctaText}
-                className="ctaButton"
-              >
-                {product.section2.ctaText} <FaArrowRightLong />
-              </Link>
-            </Col>
-          </Row>
-        </Container>
-      </section>
+              </Row>
+            )}
+          </Container>
+        </section>
+      )}
+
+      {/* Section - 3 Product Gallery */}
+      {product.section3 && (
+        <section>
+          <Container>
+            <Row>
+              <Col>
+                <span>{product.section3.subheading}</span>
+                <h2>{product.section3.title}</h2>
+                <hr />
+              </Col>
+            </Row>
+            {product.section3.gallery?.length > 0 && (
+              <Row>
+                {product.section3.gallery.map((gallery, index) => (
+                  <Col
+                    key={index}
+                    xl={4}
+                    lg={4}
+                    md={6}
+                    sm={12}
+                    xs={12}
+                    className="mb-3"
+                  >
+                    <Image
+                      src={gallery.image}
+                      alt="Gallery Item"
+                      title="Gallery Item"
+                      width={350}
+                      height={350}
+                      style={{ width: "100%", height: "auto" }}
+                      className="img-fluid rounded"
+                    />
+                  </Col>
+                ))}
+              </Row>
+            )}
+            {product.section3.ctaLink && product.section3.ctaText && (
+              <Row>
+                <Col className="text-center">
+                  <Link
+                    href={product.section3.ctaLink}
+                    title={product.section3.ctaText}
+                    className="ctaButton"
+                  >
+                    {product.section3.ctaText} <FaArrowRightLong />
+                  </Link>
+                </Col>
+              </Row>
+            )}
+          </Container>
+        </section>
+      )}
 
       {/* Service Marquee Section */}
       <ServiceMarquee />
