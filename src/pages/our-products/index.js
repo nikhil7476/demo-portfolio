4140b41baedf1a807a62768ce09ec3ab43e9c4e1
nikhil7476 <nikhil9027917476@gmail.com>
2025-10-27 19:42:27 +0530

    modified:   src/pages/our-products/index.js

--- src/pages/our-products/index.js
+++ src/pages/our-products/index.js
@@ -25,113 +25,141 @@ function OurProducts() {
       <Banner data={bannerData.products.main} />
 
       {/* Featured Products Section */}
-      <section>
-        <Container>
-          <Row>
-            <Col>
-              <span>{productData.productMain.subheading}</span>
-              <h2>{productData.productMain.title}</h2>
-              <hr />
-              {productData.myproducts.map((product, index) => (
-                <Row
-                  key={product.id}
-                  className="p-3 my-4 mx-2 bg-dark shadow rounded"
-                >
-                  {index % 2 === 0 ? (
-                    <>
-                      <Col
-                        xl={5}
-                        lg={5}
-                        md={6}
-                        sm={12}
-                        xs={12}
-                        className="mb-3"
-                      >
-                        <Image
-                          src={product.featuredImage}
-                          alt={product.title}
-                          title={product.title}
-                          width={1000}
-                          height={666}
-                          style={{ width: "100%", height: "auto" }}
-                          className="img-fluid rounded"
-                        />
-                      </Col>
-                      <Col
-                        xl={7}
-                        lg={7}
-                        md={6}
-                        sm={12}
-                        xs={12}
-                        className="mb-3"
-                      >
-                        <span className="fs-1 ">{product.icon}</span>
-                        <h3 className="my-2">{product.title}</h3>
-                        <p>{product.description}</p>
-                        <Row>
-                          <Col className="text-center ">
-                            <Link
-                              href={`/our-products/${product.slug}`}
-                              title="View Product"
-                              className="ctaButton"
-                            >
-                              View Product <FaArrowRightLong />
-                            </Link>
-                          </Col>
-                        </Row>
-                      </Col>
-                    </>
-                  ) : (
-                    <>
-                      <Col
-                        xl={7}
-                        lg={7}
-                        md={6}
-                        sm={12}
-                        xs={12}
-                        className="mb-3"
-                      >
-                        <span className="fs-1">{product.icon}</span>
-                        <h3 className="my-2">{product.title}</h3>
-                        <p>{product.description}</p>
-                        <Row>
-                          <Col className="text-center">
-                            <Link
-                              href={`/our-products/${product.slug}`}
-                              title="View Product"
-                              className="ctaButton"
-                            >
-                              View Product <FaArrowRightLong />
-                            </Link>
-                          </Col>
-                        </Row>
-                      </Col>
-                      <Col
-                        xl={5}
-                        lg={5}
-                        md={6}
-                        sm={12}
-                        xs={12}
-                        className="mb-3"
-                      >
-                        <Image
-                          src={product.featuredImage}
-                          alt={product.title}
-                          title={product.title}
-                          width={1000}
-                          height={666}
-                          style={{ width: "100%", height: "auto" }}
-                          className="img-fluid rounded"
-                        />
-                      </Col>
-                    </>
-                  )}
-                </Row>
-              ))}
-            </Col>
-          </Row>
-        </Container>
-      </section>
+      {productData?.productMain && productData?.myproducts?.length > 0 && (
+        <section>
+          <Container>
+            <Row>
+              <Col>
+                {productData.productMain.subheading && (
+                  <span>{productData.productMain.subheading}</span>
+                )}
+                {productData.productMain.title && (
+                  <h2>{productData.productMain.title}</h2>
+                )}
+                {(productData.productMain.subheading ||
+                  productData.productMain.title) && <hr />}
+
+                {productData.myproducts.map((product, index) => (
+                  <Row
+                    key={product.id || index}
+                    className="p-3 my-4 mx-2 bg-dark shadow rounded"
+                  >
+                    {index % 2 === 0 ? (
+                      <>
+                        <Col
+                          xl={5}
+                          lg={5}
+                          md={6}
+                          sm={12}
+                          xs={12}
+                          className="mb-3"
+                        >
+                          {product.featuredImage && (
+                            <Image
+                              src={product.featuredImage}
+                              alt={product.title || "Product Image"}
+                              title={product.title || "Product Image"}
+                              width={1000}
+                              height={666}
+                              style={{ width: "100%", height: "auto" }}
+                              className="img-fluid rounded"
+                            />
+                          )}
+                        </Col>
+
+                        <Col
+                          xl={7}
+                          lg={7}
+                          md={6}
+                          sm={12}
+                          xs={12}
+                          className="mb-3"
+                        >
+                          {product.icon && (
+                            <span className="fs-1">{product.icon}</span>
+                          )}
+                          {product.title && (
+                            <h3 className="my-2">{product.title}</h3>
+                          )}
+                          {product.description && <p>{product.description}</p>}
+
+                          {product.slug && (
+                            <Row>
+                              <Col className="text-center">
+                                <Link
+                                  href={`/our-products/${product.slug}`}
+                                  title="View Product"
+                                  className="ctaButton"
+                                >
+                                  View Product <FaArrowRightLong />
+                                </Link>
+                              </Col>
+                            </Row>
+                          )}
+                        </Col>
+                      </>
+                    ) : (
+                      <>
+                        <Col
+                          xl={7}
+                          lg={7}
+                          md={6}
+                          sm={12}
+                          xs={12}
+                          className="mb-3"
+                        >
+                          {product.icon && (
+                            <span className="fs-1">{product.icon}</span>
+                          )}
+                          {product.title && (
+                            <h3 className="my-2">{product.title}</h3>
+                          )}
+                          {product.description && <p>{product.description}</p>}
+
+                          {product.slug && (
+                            <Row>
+                              <Col className="text-center">
+                                <Link
+                                  href={`/our-products/${product.slug}`}
+                                  title="View Product"
+                                  className="ctaButton"
+                                >
+                                  View Product <FaArrowRightLong />
+                                </Link>
+                              </Col>
+                            </Row>
+                          )}
+                        </Col>
+
+                        <Col
+                          xl={5}
+                          lg={5}
+                          md={6}
+                          sm={12}
+                          xs={12}
+                          className="mb-3"
+                        >
+                          {product.featuredImage && (
+                            <Image
+                              src={product.featuredImage}
+                              alt={product.title || "Product Image"}
+                              title={product.title || "Product Image"}
+                              width={1000}
+                              height={666}
+                              style={{ width: "100%", height: "auto" }}
+                              className="img-fluid rounded"
+                            />
+                          )}
+                        </Col>
+                      </>
+                    )}
+                  </Row>
+                ))}
+              </Col>
+            </Row>
+          </Container>
+        </section>
+      )}
 
       {/* Service Marquee Section */}
       <ServiceMarquee />
